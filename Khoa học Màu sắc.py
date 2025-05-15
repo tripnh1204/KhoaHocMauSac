0324b984c86c9ad973b9b1c4c165f36096bb2caf
tripnh1204 <demonpham638@gmail.com>
2025-05-15 09:29:22 +0700

    Update Khoa học Màu sắc.py

--- Khoa học Màu sắc.py
+++ Khoa học Màu sắc.py
@@ -3,8 +3,12 @@ st.set_page_config(
     page_title="Khoa học Màu sắc", layout="wide"
 )
 
-st.write("### Chào mừng bạn đến với project Khoa học Màu sắc của tụi mình!")
-st.write("# Nhóm tụi mình gồm:")
-st.write("# Phạm Nguyễn Hữu Trí, MaSV 22158100")
-st.write("# Đào Nguyễn Ngọc Linh,MaSV 22158064")
+col1, col2 = st.columns([4, 1])
+with col1:
+    st.write("### Chào mừng bạn đến với project Khoa học Màu sắc của tụi mình!")
+    st.write("# Nhóm tụi mình gồm:")
+    st.write("- Phạm Nguyễn Hữu Trí, MaSV 22158100")
+    st.write("- Đào Nguyễn Ngọc Linh, MaSV 22158064")
 
+with col2:
+    st.image("logo-hcmute.jpg", width=120)
